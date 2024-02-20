90bb7e4d9b8e40780b35a57294156c77fd78215e
deokgeneral <alsejrrl@naver.com>
2024-02-20 10:42:54 +0900

    new

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>안녕하세요 바이트 테스트입니다.</h1>
+      <h1>애플티비입니다.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL:"https//api.themoviedb.org/3",
+    params:{
+        api_key: "3e19b7d9c4ae4fa5ce86714f41036a5d",
+        language : "ko-KR"
+    }
+})
+
+export default instance;
